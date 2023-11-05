94d69c39e55fb36193d74cf0b25598bc40fc83f6
asrytikov <asrytikov@gmail.com>
2023-11-05 10:34:16 +0400

    Initial commit

---
+++ src/p1/Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       try (Socket socket = new Socket("time-a.nist.gov", 13);
+            Scanner in = new Scanner(socket.getInputStream(), "UTF-8")
+       ){
+           while (in.hasNextLine()){
+               String line = in.nextLine();
+               System.out.println(line);
+           }
+       }
+    }
+}
